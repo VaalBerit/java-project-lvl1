8192b8056b1be551502e3815fd43a62733dbecf8
Peter Tolmatov <tolmatov-peter1998@yandex.ru>
2021-06-25 16:50:29 +0300

    add Cli with method

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,15 @@
 package hexlet.code;
 
+import java.util.Scanner;
+import hexlet.code.*;
+
+import static hexlet.code.Cli.requestName;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Welcome to the Brain Games!");
+    greeting();
+    Cli.greetingName(Cli.getName());
   }
+  private static void greeting() { System.out.println("Welcome to the Brain Games!"); }
+
 }
