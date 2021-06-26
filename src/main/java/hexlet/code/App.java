30377ee17e186e304a0714caf3f4a5c4807c26ba
Peter Tolmatov <tolmatov-peter1998@yandex.ru>
2021-06-26 20:39:36 +0300

    refactoring Cli

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,10 +2,8 @@ package hexlet.code;
 
 public class App {
   public static void main(String[] args) {
-    greeting();
-    Cli.greetingName(Cli.getName());
-  }
-  private static void greeting() {
-    System.out.println("Welcome to the Brain Games!"); }
+    Cli cline = new Cli();
 
+    cline.helloName(cline.getName());
+  }
 }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,24 +3,27 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
+    private static String greeting = "Welcome to the Brain Games!" + "\n" + "May I have your name? ";
     private static String name = requestName();
 
-    protected static String requestName() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("May I have your name? ");
-
-        String nameTemp = scanner.nextLine();
-
-        while (nameTemp.isEmpty()) {
-            System.out.println("You didn't enter a name or specified something bad! \n Try again: ");
-            nameTemp = scanner.nextLine();
-        }
-        return nameTemp;
-    }
     public static String getName() {
         return name;
     }
-    public static void greetingName(final String pName) {
-        System.out.println("Hello, " + pName + "!");
+
+    public static void getGreeting() {
+        System.out.print(greeting); }
+
+    public static void helloName(final String pName) { System.out.println("Hello, " + pName + "!"); }
+
+    protected static String requestName() {
+        Scanner scanner = new Scanner(System.in);
+        getGreeting();
+        String nameTemp = scanner.nextLine();
+
+        while (nameTemp.isEmpty()) {
+            System.out.println("You didn't enter a name or specified something bad!" + "\n" + "Try again: ");
+            nameTemp = scanner.nextLine();
+        }
+        return nameTemp;
     }
 }
