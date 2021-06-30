277290b502d4c125a5f6bb877ba022e34ac65b25
Peter Tolmatov <tolmatov-peter1998@yandex.ru>
2021-06-30 16:58:54 +0300

    add RequestAnswer.java

---
+++ src/main/java/hexlet/code/gamecontroller/RequestAnswer.java
@@ -0,0 +1,25 @@
+package hexlet.code.gamecontroller;
+
+import java.util.Scanner;
+
+public class RequestAnswer {
+    private static String answer = "";
+
+    public static String requestInput() {
+        Scanner scanner = new Scanner(System.in);
+        while (answer.isEmpty()) {
+            if (scanner.hasNextLine() &&
+                    (scanner.hasNext("yes") || scanner.hasNext("no")))
+            {
+                answer = scanner.nextLine();
+            } else {
+                System.out.println("You answered incorrectly, only \"yes\" or \"no\" are accepted!" + "\n" + "Try again: ");
+                scanner.nextLine();
+            }
+        }
+        return answer;
+    }
+    public void resetAnswer() {
+        answer = "";
+    }
+}
