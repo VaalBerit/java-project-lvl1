acefd07faa358c0e7c04da3a03a06ee1fab59e77
Peter Tolmatov <tolmatov-peter1998@yandex.ru>
2021-06-30 16:56:05 +0300

    refactoring RequestNumber

--- src/main/java/hexlet/code/gamecontroller/RequestNumber.java
+++ src/main/java/hexlet/code/gamecontroller/RequestNumber.java
@@ -8,7 +8,7 @@ public class RequestNumber {
     public static int requestInput() {
         Scanner scanner = new Scanner(System.in);
             if (scanner.hasNextInt()) {
-                answer = (scanner.nextInt());
+                answer = scanner.nextInt();
             }
         return answer;
     }
