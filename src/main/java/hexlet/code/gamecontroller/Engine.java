1ee6a6f08dc57d7b859589bc7b66609d4bf296e0
Peter Tolmatov <tolmatov-peter1998@yandex.ru>
2021-07-02 19:53:32 +0300

    refactoring Engine

--- src/main/java/hexlet/code/gamecontroller/Engine.java
+++ src/main/java/hexlet/code/gamecontroller/Engine.java
@@ -1,21 +1,25 @@
 package hexlet.code.gamecontroller;
 
+import hexlet.code.games.Calculator;
 import hexlet.code.games.Cli;
 import hexlet.code.games.Exit;
 import hexlet.code.games.Even;
-import hexlet.code.view.GameList;
+import hexlet.code.view.GamesList;
 
 public class Engine extends  RequestNumber {
     public static void gamesSelector(int answer) {
-        if (answer >= 0 && answer < GameList.getBrainGamesName().size()) {
-            switch (answer) {
-                case 1:
+        String strAnswer = String.valueOf(answer);
+        if (answer >= 0 && answer < GamesList.getBrainGamesName().size()) {
+            switch (strAnswer) {
+                case "1":
                     Cli.runGameGreet();
                     break;
-                case 2:
+                case "2":
                     Even.runGameEven();
                     break;
-                case 0: Exit.exitGame();
+                case "3":
+                    Calculator.runGameCalc();
+                case "0": Exit.exitGame();
                 default: break;
             }
         } else {
